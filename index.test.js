de68de101ff7cdc43d17bd300c5e347835524b09
illuSioN4ng <413972057@qq.com>
2019-07-08 15:21:22 +0800

    :sparkles: feat - add mochawesome

--- index.test.js
+++ index.test.js
@@ -3,7 +3,7 @@ let { expect } = require('chai'),
   _ = require('lodash')
 
 const { alwaysTrue, legitString, errorLegitString } = require('./index')
-
+import { getPerson, Person } from './person'
 describe('#mocha basics', () => {
   it('true should be true', () => {
     true.should.be.true
@@ -53,7 +53,7 @@ describe('#legitString', () => {
     expect(legitString(null)).to.be.false
   })
 })
-
+// * 错误代码
 // describe('#errorLegitString', () => {
 //   it('cow should be a string', () => {
 //     errorLegitString('cow').should.be.true
@@ -71,3 +71,31 @@ describe('#legitString', () => {
 //     expect(errorLegitString(null)).to.be.false
 //   })
 // })
+
+describe('#initial person condition', () => {
+  it('initial person should be an object', () => {
+    expect(_.isObject(getPerson())).to.be.true
+  })
+
+  it('armorBouns by default is 2 wearing leatherArmor', () => {
+    const person = getPerson()
+    person.armorBonus.should.equal(2)
+  })
+})
+
+describe('#Person', () => {
+  describe('#rollDice', () => {
+    it('should return a finite number (not NaN nor Infinity)', () => {
+      const number = Person.rollDice(1, 20)
+      _.isFinite(number).should.be.true
+    })
+
+    it('in a 10000 times samples, there should be no 0 in result', () => {
+      const samples = new Array(10000),
+        rollDiceSamples = _.fill(samples, 0).map(() => Person.rollDice(1, 20))
+
+      // console.log(rollDiceSamples)
+      rollDiceSamples.filter(item => item === 0).length.should.equal(0)
+    })
+  })
+})
